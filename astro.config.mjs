66be386f6e32e1e1d7aab9162fed0007252510f6
elsahv <elsahvy@gmail.com>
2024-02-15 17:02:57 -0800

    vercel astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+// Import /serverless for a Serverless SSR site
+import vercelServerless from "@astrojs/vercel/serverless";
 
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercelServerless(),
 });
