cb67d846428b6441b57966134554a57ec3d69871
elsahv <elsahvy@gmail.com>
2024-02-15 22:41:14 -0800

    works mobile css update- end

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-// Import /serverless for a Serverless SSR site
+import netlify from "@astrojs/netlify";
 
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [tailwind()],
-
   output: "server",
-  adapter: vercel(),
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
 });
