57c1762833d71976e35f8177b0724e5abced8e07
elsahv <elsahvy@gmail.com>
2025-02-07 19:42:57 -0800

    css mobile fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      testWidth: {
+        testCustomSm: "599px",
+      },
       colors: {
         richBlack: "#001524",
         lavender: "#CDD1E2",
