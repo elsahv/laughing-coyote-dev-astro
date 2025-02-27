66a6976ed7be7b5d1dfca1df3f3ba7d2a468dde9
elsahv <elsahvy@gmail.com>
2025-02-26 22:24:51 -0800

    mobile css  progress

--- src/components/ctas/Testimonial.jsx
+++ src/components/ctas/Testimonial.jsx
@@ -1,8 +1,14 @@
 const Testimonial = () => {
   return (
-    <div id="testimonial-wrapper" class="m-[250px] ">
-      <div id="" class=" flex">
-        <div id="sq1" class="bg- flex justify-end items-center bg- p-5">
+    <div
+      id="testimonial-wrapper"
+      class="bg- 2xl:m-[250px] md:my-[150px] md:mx-[50px] -"
+    >
+      <div id="" class="flex md:flex-row flex-col">
+        <div
+          id="sq1"
+          class="bg- flex md:justify-end justify-center items-center bg- p-5"
+        >
           <p className="w-3/4 font-medium text-lg">
             "client side, site-Lorem ipsum, dolor sit amet consectetur
             adipisicing elit. Soluta sit laboriosam deleniti, consectetur
@@ -12,7 +18,7 @@ const Testimonial = () => {
         </div>
         <div
           id="sq2"
-          class="bg- flex justify-start items-center text-onyx font-light "
+          class="bg- flex md:justify-start justify-center items-center text-onyx font-light "
         >
           <img
             src="https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
