e462f24092f0a066f3a4af3794698a9ef28b1fc4
elsahv <elsahvy@gmail.com>
2025-03-22 02:30:25 -0700

    new works cta section test

--- src/components/sections/SocialProof/index.jsx
+++ src/components/sections/SocialProof/index.jsx
@@ -8,14 +8,7 @@ import Sq6 from "./sq6";
 const SocialProof = () => {
   return (
     <div id="masonryGrid-wrapper" class="2xl:opacity-80 opacity-100 bg-">
-      {/* <div id="masonryGrid-container" className="opacity-90 hover:opacity-100"> */}
       <div id="" className="bg- md:mx-5 mx-0 2xl:px-[80px]">
-        {/* <div id="" class="flex justify-center p-3">
-            <h2 class="underline text-3xl pb-5 font-bold italic md:mx-5 md:px-5">
-              Social Proof- Website Case Studies
-            </h2>
-          </div> */}
-
         <div
           id="grid"
           className="bg- grid 2xl:grid-cols-3 md:grid-cols-2 grid-cols-1 2xl:gap-2 2xl:px-[100px] p-7"
@@ -31,12 +24,10 @@ const SocialProof = () => {
             class="flex md:flex-row flex-col md:space-x-4 col-span-3"
           >
             <Sq5 />
-
             <Sq6 />
           </div>
         </div>
       </div>
-      {/* </div> */}
     </div>
   );
 };
--- src/components/sections/SocialProof/sq3.jsx
+++ src/components/sections/SocialProof/sq3.jsx
@@ -4,11 +4,16 @@ const Sq3 = () => {
   return (
     <div
       id="sq3"
-      className="lg:mt-[190px] md:w-[445px] md:h-[205px]  md:ml-5 rounded-lg p-0"
+      className="lg:mt-[190px] md:w-[465px] md:h-[205px] md:ml-5 rounded-lg"
     >
-      <h4 className="">Social Media</h4>
+      {/* <h4 className="">Social Media</h4> */}
       <div className="text-test2  bg-teal sq-styles">
-        <WindowGrid />
+        {/* <WindowGrid /> */}
+        <ul class="pl-3 pt-3 pb-3 text-2xl space-y-4">
+          <li>web design</li>
+          <li>software development</li>
+          <li>copywriting</li>
+        </ul>
       </div>
     </div>
   );
