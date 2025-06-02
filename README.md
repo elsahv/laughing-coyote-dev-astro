a57d3ccdeb957a00af71815e50a8f4892dbdd8ef
elsahv <elsahvy@gmail.com>
2025-06-01 22:13:58 -0700

    finalize sections

--- README.md
+++ README.md
@@ -1,23 +1,19 @@
 - TREE VIEW:
   |
-- 1. HERO
-     |
-- 2. BUILD AUTHORITY- listed customers
-     |
-- 3. ADDRESS KEY PROBLEM- what problem LC solves  
-     |
-- 4. SOCIAL PROOF- provide solutions, glowing testimonial- " Before I was  
-     struggling with [problem,] and it made me feel [negative emotion]. Now I have acheived this [result], and I feel [positive emotion]."
-     |
-- 5. HOW IT WORKS- gives client a plan, include CTA button
-     |
-- 6. PORTFOLIO EXAMPLES- https://www.youtube.com/watch?v=Ny-Az8nAAaM&t=1010s-  
-     17:25-17:38
-     |
-- 7. PACKAGES
-     |
-- 8. BUILD AUTHORITY- 19:40-21:03
-     |
-  9. FAQ
-     |
-  10. FINAL CTA- 21:05
+  _1.HERO_
+  |
+  _2. BUILD AUTHORITY- listed customers_
+  |
+  _3. ADDRESS KEY PROBLEM- what problem LC solves_  
+   |
+  _4.SOCIAL PROOF- provide solutions, glowing testimonial- " Before I was struggling with [problem,] and it made me feel [negative emotion]. Now I have acheived this [result], and I feel [positive emotion]." (USE <FeaturedClient />)_
+  |
+  _5. HOW IT WORKS- gives client a plan, include CTA button_
+  |
+  _6. PORTFOLIO EXAMPLES- https://www.youtube.com/watch?v=Ny-Az8nAAaM&t=1010s- 17:25-17:38_
+  |
+  _7. PACKAGES_
+  |
+  _8. BUILD AUTHORITY (one final title, paragraph) - 19:40-21:03_
+  <!-- | 9. FAQ -->
+  | 10. FINAL CTA- 21:05
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -11,7 +11,6 @@ const items = [
   {
     btn: "react",
   },
-
   {
     btn: "content",
   },
@@ -22,59 +21,61 @@ const items = [
 
 const ContactForm = () => {
   return (
-    <div id="contact" class="mt-1 pt-3 lg:p-5">
-      <form id="IP-login-form" class="pt-5 bg-test flex flex-col gap-y-4">
-        <input
-          placeholder="enter email"
-          class="border-b border-carribean bg-aliceBlue md:w-2/3 w-1/2 h-10 pl-2 hover:border-test1 mb-2"
-        />
-        <input
-          placeholder="enter password"
-          class="border-b border-carribean bg-aliceBlue md:w-2/3 w-1/2 h-10 pl-2 hover:border-test2"
-        />
-        <div id="btns" class="">
-          <h3 class="py-3 text-xl">I'm looking for...</h3>
+    <div className="text-teal bg-parchment rounded-tl-2xl rounded-bl-2xl pl-5 ml-5">
+      <div id="contact" class="mt-1 pt-3 lg:p-5">
+        <form id="IP-login-form" class="pt-5 bg-test flex flex-col gap-y-4">
+          <input
+            placeholder="enter email"
+            class=" bg-teal md:w-2/3 w-1/2 h-10 pl-2 hover:border-test1 mb-2"
+          />
+          <input
+            placeholder="enter password"
+            class="border-b border-carribean bg-teal md:w-2/3 w-1/2 h-10 pl-2 hover:border-test2"
+          />
+          <div id="btns" class="">
+            <h3 class="py-3 text-xl">I'm looking for...</h3>
 
-          <div
-            id="btns-container"
-            class="text-sm md:w-2/3 pt-1 mt-5 pr-4 grid 2xl:grid-cols-3 grid-cols-2 gap-6"
-          >
-            {items.map((item) => (
-              <button
-                id={item.btn}
-                className="w-[139px] hover:bg-onyx hover:text-aliceBlue  rounded border border-black mr-5 py-1 pl-2 text-center"
-              >
-                {item.btn}
-              </button>
-            ))}
+            <div
+              id="btns-container"
+              class="text-sm md:w-2/3 pt-1 mt-5 pr-4 grid 2xl:grid-cols-3 grid-cols-2 gap-6"
+            >
+              {items.map((item) => (
+                <button
+                  id={item.btn}
+                  className="w-[139px] bg-teal text-parchment hover:bg-onyx hover:text-aliceBlue  rounded  border-black mr-5 py-1 pl-2 text-center"
+                >
+                  {item.btn}
+                </button>
+              ))}
+            </div>
           </div>
-        </div>
-        <div id="wrapper" class="mt-[50px] bg- 2xl:flex items-center">
-          <p class="">How would you describe your project?</p>
-          <div className="">
-            <textarea
-              placeholder="ex: what are your business goals?"
-              class="pt-5 pl-2 w-[500px] border border-black bg-aliceBlue 2xl:ml-5 2xl:mt-1 mt-4"
-            />
+          <div id="wrapper" class="mt-[50px] bg- 2xl:flex items-center">
+            <p class="">How would you describe your project?</p>
+            <div className="">
+              <textarea
+                placeholder="ex: what are your business goals?"
+                class="pt-5 pl-2 w-[500px] border border-black bg-aliceBlue 2xl:ml-5 2xl:mt-1 mt-4"
+              />
+            </div>
           </div>
-        </div>
-        <div id="btn-wrapper" class="">
-          <button class="rounded bg-munsell text-aliceBlue px-3 py-1 border border-carribean opacity-80 hover:opacity-100">
-            send
-          </button>
-        </div>
-      </form>
+          <div id="btn-wrapper" class="">
+            <button class="rounded bg-munsell text-aliceBlue px-3 py-1 border border-carribean opacity-80 hover:opacity-100">
+              send
+            </button>
+          </div>
+        </form>
 
-      <div id="all-contact-info" class="mt-5 bg-">
-        <div id="email" class="">
-          elsahvy@gmail.com
-        </div>
-        <div id="sm-container">
-          <ul id="sm-links" class="flex gap-x-5 pt-1 text-md opacity-80 pl-1">
-            <li>linkedIn</li>
-            <li>fb</li>
-            <li>inst</li>
-          </ul>
+        <div id="all-contact-info" class="mt-5 bg-">
+          <div id="email" class="">
+            elsahvy@gmail.com
+          </div>
+          <div id="sm-container">
+            <ul id="sm-links" class="flex gap-x-5 pt-1 text-md opacity-80 pl-1">
+              <li>linkedIn</li>
+              <li>fb</li>
+              <li>inst</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
